911543ca2adb7075bde703fa1266e700bad63e93
ekinohito <zhenyagur9@gmail.com>
2021-12-04 01:37:23 +0300

    rubbish classifier

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ from number_reader import Detector
 from itertools import cycle
 from asyncio import sleep
 
+from rubbish_classifier.inference import predictions
+
 app = FastAPI()
 app.mount("/public", StaticFiles(directory="public"), name="public")
 number_detector = Detector(r'--oem 3 --psm 11 outputbase digits')
 side_images = [cv2.imread('photos/num1.jpg'), cv2.imread('photos/num2.jpg'), cv2.imread('photos/num3.jpg')]
+top_images = [[cv2.imread('photos/test1.jpg')], [cv2.imread('photos/test2.jpg')], [cv2.imread('photos/test3.jpg')]]
 current_side_image = None
+current_top_image = None
 
 @app.get("/")
 async def root():
@@ -24,14 +28,29 @@ counter = 0
 async def get_number():
     global counter
     counter += 1
+    counter %= 1000
     cv2.imwrite(f'public/{counter}.jpg', number_detector.save_image(current_side_image))
     return {"url": f'public/{counter}.jpg', "code": number_detector.get_number(current_side_image)}
 
 
+@app.get("/frame")
+async def get_frame():
+    global counter
+    counter += 1
+    counter %= 1000
+    probability = predictions(current_top_image)
+    cv2.imwrite(f'public/{counter}.jpg', current_top_image)
+    return {"url": f'public/{counter}.jpg', "probability": probability}
+
+
+
 async def iterate():
     global current_side_image
-    for img in cycle(side_images):
-        current_side_image = img
-        await sleep(1)
+    global current_top_image
+    for side_img, top_imgs in cycle(zip(side_images, top_images)):
+        current_side_image = side_img
+        for top_img in top_imgs:
+            current_top_image = top_img
+            await sleep(3)
 
 create_task(iterate())
---
+++ rubbish_classifier/inference.py
@@ -0,0 +1,27 @@
+import cv2
+import torch
+from .model import model
+from .data_transform import transform_test
+
+
+def predictions(image, label=False):
+    # image = Image.open(image)
+    # image = cv2.imread(image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = transform_test(image=image)['image']
+
+    output = model(image[None, ...])
+
+    if label:
+      predicted = torch.round(output)
+      predicted = predicted.squeeze().tolist()
+
+      return predicted
+
+    predicted_proba = round(output.squeeze().tolist() * 100, 2)
+    return predicted_proba
+
+
+if __name__ == '__main__':
+    res = predictions(cv2.imread('photos/test1.jpg'))
+    print("Вероятность брака: {0} %".format(res))
